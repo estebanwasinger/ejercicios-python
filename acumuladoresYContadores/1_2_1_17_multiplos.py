f2f488904fc5f5a4c8edd51ce4860a3b66db06d7
Esteban Wasinger <estebanwasinger@gmail.com>
2022-06-05 17:32:32 -0700

    Resueltos con flor

--- acumuladoresYContadores/1_2_1_17_multiplos.py
+++ acumuladoresYContadores/1_2_1_17_multiplos.py
@@ -3,3 +3,34 @@
 # Dado un valor numérico entero positivo n, informar los primeros n múltiplos de 5
 # que no sean múltiplos de 3.
 # Por ejemplo: si n fuera 6, la salida deberá ser: 5, 10, 20, 25, 35, 40.
+
+from utils import read_int
+
+n = read_int("Ingrese un número entero positivo: ")
+
+cantMultiplosEncontrados : int = 0
+i : int = 1
+
+# Cuando "cantMultiplosEncontrados" sea igual a N hay que cortar
+# while cantMultiplosEncontrados < n:
+#     print("Se hizo una vuelta")
+#     esIMultiplo = i % 5 == 0 # Si I es multiplo o no de 5
+#
+#     if esIMultiplo:
+#         if not (i % 3 == 0):
+#             cantMultiplosEncontrados = cantMultiplosEncontrados + 1
+#             print(i)
+#
+#     i = i + 1
+
+while cantMultiplosEncontrados < n:
+    print("Se hizo una vuelta")
+    multiploDe5 = i * 5 # voy calculando los múltplos
+
+
+    if not (multiploDe5 % 3 == 0):
+        cantMultiplosEncontrados = cantMultiplosEncontrados + 1
+        print(multiploDe5)
+
+    i = i + 1
+
--- acumuladoresYContadores/1_2_1_20_cantidades_promedios_y_porcentajes.py
+++ acumuladoresYContadores/1_2_1_20_cantidades_promedios_y_porcentajes.py
@@ -2,10 +2,45 @@
 # Dificultad: básica, Requerido: Indispensable.
 # Se ingresan varios valores numéricos enteros, finalizando la carga de datos al ingresar
 # un 0 (cero). Se pide informar:
-# 1. Cantidad positivos.
-# 2. Cantidad de negativos.
-# 3. Porcentaje de pares.
-# 4. Promedio de los positivos.
+# 1. Cantidad positivos. *
+# 2. Cantidad de negativos. *
+# 3. Porcentaje de números pares. *
+# 4. Promedio de los positivos. *
 # 5. Porcentaje de negativos.
 
 from utils import read_int
+
+num = read_int("Ingrese números enteros. Ingrese 0 para finalizar.")
+# Contadores
+cantidadPositivos : int = 0
+cantidadNegativos : int = 0
+cantidadPares : int = 0
+
+#Acumulador
+totalPositivos : int = 0
+
+while num != 0:
+    if(num % 2 == 0):
+        cantidadPares = cantidadPares + 1
+
+    if(num > 0):
+        cantidadPositivos = cantidadPositivos + 1
+        totalPositivos = totalPositivos + num
+
+    if(num < 0):
+        cantidadNegativos = cantidadNegativos + 1
+
+    num = read_int("Ingrese otro número:")
+
+print("La cantidad de positivos es: ")
+print(cantidadPositivos)
+print("La cantidad de negativos es: ")
+print(cantidadNegativos)
+print("El porcentaje de pares es:")
+print(cantidadPares / (cantidadNegativos + cantidadPositivos) * 100)
+print("El promedio de positivos es:")
+print(totalPositivos / cantidadPositivos)
+print("El porcentaje de negativos es:")
+print(cantidadNegativos / (cantidadNegativos + cantidadPositivos) * 100)
+
+
